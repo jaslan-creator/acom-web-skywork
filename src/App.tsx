16eaa4c6dd0fbde07f133e8d45e564ff12b55607
jaslan-creator <232265434+jaslan-creator@users.noreply.github.com>
2026-02-05 19:56:37 -0500

    feat: add Vercel Speed Insights
    - Installed @vercel/speed-insights package
    - Added SpeedInsights component to App.tsx for performance monitoring

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { ROUTE_PATHS } from "@/lib/index";
 import { Layout } from "@/components/Layout";
 import Home from "@/pages/Home";
@@ -80,6 +81,7 @@ const App = () => {
             </Routes>
           </Layout>
         </BrowserRouter>
+        <SpeedInsights />
       </TooltipProvider>
     </QueryClientProvider>
   );
